fcca5a7df73a3bf4a04427d141f44df34c022141
volemar264@gmail.com <volemar264@gmail.com>
2021-04-25 17:33:39 +0300

    Imported animations asset pack, added hit animation and health

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,15 +7,23 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] private float weaponRange = 2f;
+        [SerializeField] private float timeBetweenAttacks = 1f;
+        [SerializeField] private float weaponDamage = 2f;
+
+        float timeSinceLastAttack = 0;
 
         Transform target;
         Mover mover;
         ActionScheduler action;
+        Animator animator;
         private void Awake() {
             mover = GetComponent<Mover>();
+            animator = GetComponent<Animator>();
         }
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if(target == null) return;
             if (!GetIsInRange())
             {
@@ -24,7 +32,17 @@ namespace RPG.Combat
             else
             {
                 mover.Cancel();
+                if(timeSinceLastAttack >= timeBetweenAttacks)
+                {
+                    AttackBehaviour();
+                    timeSinceLastAttack = 0;
+                }
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            animator.SetTrigger("attack");
         }
 
         private bool GetIsInRange()
@@ -41,6 +59,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        //Animation event
+        private void Hit()
+        {
+            target.GetComponent<Health>().TakeDamage(weaponDamage);
         }
     }
 }
